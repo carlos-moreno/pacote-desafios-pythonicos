d5c100e6ad87d4b399c0e7f87ded5165fbcd789c
Carlos Moreno <omorenodovale@gmail.com>
2020-06-27 23:00:40 -0500

    05_verbing.py exercise solution

--- 05_verbing.py
+++ 05_verbing.py
@@ -10,12 +10,15 @@ Se o tamanho da string for menor que 3, não altere nada.
 Retorne o resultado da string.
 """
 
+
 def verbing(s):
-    # +++ SUA SOLUÇÃO +++
-    return
+    if len(s) >= 3:
+        s = f'{s}ly' if s.endswith("ing") else f'{s}ing'
+    return s
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
+
 
 def test(f, in_, expected):
     """
@@ -25,17 +28,17 @@ def test(f, in_, expected):
     out = f(in_)
 
     if out == expected:
-        sign = '✅'
-        info = ''
+        sign = "✅"
+        info = ""
     else:
-        sign = '❌'
-        info = f'e o correto é {expected!r}'
+        sign = "❌"
+        info = f"e o correto é {expected!r}"
 
-    print(f'{sign} {f.__name__}({in_!r}) retornou {out!r} {info}')
+    print(f"{sign} {f.__name__}({in_!r}) retornou {out!r} {info}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Testes que verificam o resultado do seu código em alguns cenários.
-    test(verbing, 'hail', 'hailing')
-    test(verbing, 'swiming', 'swimingly')
-    test(verbing, 'do', 'do')
+    test(verbing, "hail", "hailing")
+    test(verbing, "swiming", "swimingly")
+    test(verbing, "do", "do")
